c35eba6f77ec4bf737ef8160c9acdcc7ea6d8c23
Trần Nguyên Hào <Henryporison@outlook.com.vn>
2025-03-17 13:50:00 +0700

    done_about_left

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,31 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.scss";
-import { useEffect, useRef } from "react";
-// scrollreveal
 import ScrollReveal from "scrollreveal";
-// Tilt
-import VanillaTilt from "vanilla-tilt";
 import AlbumGallery from "../AlbumGallery/AlbumGallery";
+import videoData from "../../../public/dataVideo.json";
 
 const Home = () => {
-  // ScrollReveal
+  const [popupVideo, setPopupVideo] = useState(null);
+
   useEffect(() => {
     const sr = ScrollReveal({
-      reset: true, // Đảm bảo animation sẽ luôn thực hiện khi cuộn lại
+      reset: true,
       origin: "top",
       distance: "50px",
-      duration: "700",
+      duration: 700,
       delay: 200,
     });
 
-    sr.reveal(`.name`, { origin: "left", interval: 200 });
-    sr.reveal(`.gif`, { origin: "right", interval: 200 });
-    sr.reveal(`.note`, { origin: "bottom", interval: 200 });
-    sr.reveal(`.cv`, { origin: "bottom", interval: 200 });
-    //
-    // sr.reveal(`.projects`, { origin: "bottom", interval: 200 });
-    sr.reveal(`.item`, { origin: "top", interval: 200 });
+    sr.reveal(".name", { origin: "left", interval: 200 });
+    sr.reveal(".gif", { origin: "right", interval: 200 });
+    sr.reveal(".note", { origin: "bottom", interval: 200 });
+    sr.reveal(".cv", { origin: "bottom", interval: 200 });
+    sr.reveal(".item", { origin: "top", interval: 200 });
   }, []);
+
+  // Lấy danh sách video từ JSON
+  const videos = videoData.videos.map((video) => ({
+    ...video,
+    embedLink: video.link.replace("youtu.be/", "www.youtube.com/embed/"), // Chuyển link sang dạng nhúng
+  }));
+
+  const openPopup = (embedLink) => {
+    setPopupVideo(embedLink);
+  };
+
+  const closePopup = () => {
+    setPopupVideo(null);
+  };
 
   return (
     <div className="home_container">
@@ -34,62 +44,36 @@ const Home = () => {
           <div className="avt">
             <img
               src="https://raw.githubusercontent.com/NguyenHaoTran/IMGS_Dev_Por/refs/heads/main/avt.jpg"
-              alt=""
+              alt="Avatar"
             />
           </div>
           <h1>NguyenHaoTran</h1>
         </div>
-        <div className="slogan">
-          <p>
-            "Mỗi thước phim không chỉ là hình ảnh, mà còn là câu chuyện, cảm xúc
-            và đam mê."
-          </p>
-        </div>
       </div>
-      {/*  */}
+
+      {/* Nội dung chính */}
       <div className="contents">
         <video autoPlay loop muted playsInline className="banner_video">
-          <source src="public\banner_2359.mp4" type="video/mp4" />
+          <source src="public/banner_2359.mp4" type="video/mp4" />
           Trình duyệt của bạn không hỗ trợ video.
         </video>
-        {/* PROJECTS */}
+
+        {/* Danh sách dự án */}
         <div className="projects">
-          <p className="p_projects">
-            "Những thước phim này không chỉ là hình ảnh chuyển động, mà còn là
-            góc nhìn, cảm xúc và cách tôi kể câu chuyện của riêng mình."
-          </p>
           <div className="items_project">
-            <div className="item_project">
-              <a href="#">
-                <p>tatsemakerhub</p>
-                <img src="public\tatseMakerHub_1.1.1.png" alt="" />
-              </a>
-            </div>
-            <div className="item_project">
-              <a href="#">
-                <p>qua khung cửa sổ #2</p>
-                <img src="public\QKCS1.2_1.1.2.png" alt="" />
-              </a>
-            </div>
-            <div className="item_project">
-              <a href="#">
-                <p>qua khung cửa sổ #6</p>
-                <img src="public\QKCS_6_1.1.2.png" alt="" />
-              </a>
-            </div>
-            <div className="item_project">
-              <a href="#">
-                <p>qua khung cửa sổ #3</p>
-                <img src="public\QKCS3_1.1.1.png" alt="" />
-              </a>
-            </div>
-            <div className="item_project">
-              <a href="#">
-                <p>Trị An 2023</p>
-                <img src="public\DKM_1.1.4.png" alt="" />
-              </a>
-            </div>
+            {videos.map((video, index) => (
+              <div
+                key={index}
+                className="item_project"
+                onClick={() => openPopup(video.embedLink)}>
+                <a>
+                  <p>{video.title}</p>
+                  <img src={video.cover} alt={video.title} />
+                </a>
+              </div>
+            ))}
           </div>
+
           <div className="texts">
             <p className="p_projects">
               "Mỗi bức ảnh là một khung hình tĩnh, nơi ánh sáng, màu sắc và
@@ -97,21 +81,38 @@ const Home = () => {
             </p>
             <AlbumGallery />
           </div>
-          <div className="stills"></div>
         </div>
       </div>
+
+      {/* Footer */}
       <div className="footer">
         <div className="socials">
           <a href="https://www.facebook.com/profile.php?id=100004978517081">
             <i className="ri-facebook-box-fill"></i>
           </a>
-          <a href="https://www.instagram.com/ng.hao.t/?fbclid=IwY2xjawFWjpVleHRuA2FlbQIxMAABHTTWJkf5XziAIpi7WI_szWKaBTgqJPMZNCaRVW4TLBlPtbbydJke-DisYw_aem_ZNAD-vCGjGHxYT6F5XSoeg">
+          <a href="https://www.instagram.com/ng.hao.t/">
             <i className="ri-instagram-line"></i>
           </a>
         </div>
         <p>HCM city_VietNam</p>
         <p>trannguyenhao123@gmail.com</p>
       </div>
+
+      {/* Popup Video */}
+      {popupVideo && (
+        <div className="popup active" onClick={closePopup}>
+          <div className="popup-content">
+            <button className="close-btn" onClick={closePopup}>
+              ×
+            </button>
+            <iframe
+              src={popupVideo}
+              title="YouTube Video"
+              frameBorder="0"
+              allowFullScreen></iframe>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
