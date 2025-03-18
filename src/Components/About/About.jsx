08e9f72758f5d36228c30f16737172e57558fce9
Trần Nguyên Hào <Henryporison@outlook.com.vn>
2025-03-18 10:56:35 +0700

    18.3

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,32 +6,40 @@ import ScrollReveal from "scrollreveal";
 
 const About = () => {
   // ScrollReveal
-  useEffect(() => {
-    const sr = ScrollReveal({
-      reset: true, // Đảm bảo animation sẽ luôn thực hiện khi cuộn lại
-      origin: "top",
-      distance: "50px",
-      duration: "700",
-      delay: 200,
-    });
+  // useEffect(() => {
+  //   const sr = ScrollReveal({
+  //     reset: true, // Đảm bảo animation sẽ luôn thực hiện khi cuộn lại
+  //     origin: "top",
+  //     distance: "50px",
+  //     duration: "700",
+  //     delay: 200,
+  //   });
 
-    sr.reveal(`.name`, { origin: "left", interval: 200 });
-    //
-    sr.reveal(`.hocVan`, { origin: "top", interval: 200 });
-    sr.reveal(`.item`, { origin: "top", interval: 200 });
-  }, []);
+  //   sr.reveal(`.name`, { origin: "left", interval: 200 });
+  //   //
+  //   sr.reveal(`.hocVan`, { origin: "top", interval: 200 });
+  //   sr.reveal(`.item`, { origin: "top", interval: 200 });
+  // }, []);
 
   return (
     <div className="about_container">
+      <div className="banner">
+        <img className="img_banner" src="/bannerAbout2.png" alt="" />
+        <p>"Mọi khung hình đều có một câu chuyện."</p>
+      </div>
+
       <div className="contents">
         <div className="name">
-          <p>
-            testtesttesttesttesttesttesttesttesttesttesttesttest testtest
-            testtesttesttest test testtesttest testtest test test test
-            testtesttesttest test test test test test test testtesttesttesttest
-            test test test test
-          </p>
+          <img className="avt" src="" alt="" />
+          <div className="right">
+            <div className="right-name">
+              <h3>Trần Nguyên Hào</h3>
+              <div className="sub_title">Filmmaker, Photographer, Editor</div>
+            </div>
+          </div>
         </div>
+        <div className="texts"></div>
+        <div className="contacts"></div>
       </div>
     </div>
   );
